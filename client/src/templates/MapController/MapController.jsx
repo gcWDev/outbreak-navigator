ff08946debaed4e01cd5e91f148250b4162105ab
gcWDev <cohenguy74@gmail.com>
2023-12-03 11:08:08 -0500

    removed unneeded dependencies

--- client/src/templates/MapController/MapController.jsx
+++ client/src/templates/MapController/MapController.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styles from "./MapController.module.css";
 import { NavLink } from "react-router-dom";
-import PlacesAutocomplte from "../../components/PlacesAutocomplete";
 import DefaultOptions from "./DefaultOptions";
 import defaultPlaces from "../../utilities/defaultPlaces";
 import Filter from "./Filter";
@@ -11,90 +10,88 @@ import EastIcon from "@mui/icons-material/East";
 import { useLocation } from "react-router-dom";
 
 export default function MapController(props) {
-    const { setCoordinates, filterSet, setSelectedFilters } = props;
-    const { pathname } = useLocation();
+  const { setCoordinates, filterSet, setSelectedFilters } = props;
+  const { pathname } = useLocation();
 
-    return (
-        <div
-            className={`card ${styles.mapController} text-center`}
-            id="map-controller"
-        >
-            {/* <div className="heading border-bottom">
+  return (
+    <div
+      className={`card ${styles.mapController} text-center`}
+      id="map-controller"
+    >
+      {/* <div className="heading border-bottom">
                 <h3>This is the controller</h3>
                 <p>Lorem ipsum dolor sit amet.</p>
             </div> */}
-            <div className={styles.controlsContainer}>
-                <div
-                    // className={`mt-4 ${styles.mapTypeToggleContainer} container d-flex flex-column gap-4`}
-                    className={`${styles.mapTypeToggleContainer} container d-flex flex-column gap-4`}
-                >
-                    <div className="row">
-                        <div className="col-12">
-                            <DefaultOptions
-                                places={defaultPlaces}
-                                setCoordinates={setCoordinates}
-                            />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive
-                                    ? `col-6 ${styles.MapToggleButton} ${styles.active}`
-                                    : `col-6 ${styles.MapToggleButton}`
-                            }
-                            to="/places"
-                        >
-                            Places
-                        </NavLink>
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive
-                                    ? `col-6 ${styles.MapToggleButton} ${styles.active}`
-                                    : `col-6 ${styles.MapToggleButton}`
-                            }
-                            to="/danger-zones"
-                        >
-                            Danger Zones
-                        </NavLink>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            {/* <PlacesAutocomplte
+      <div className={styles.controlsContainer}>
+        <div
+          // className={`mt-4 ${styles.mapTypeToggleContainer} container d-flex flex-column gap-4`}
+          className={`${styles.mapTypeToggleContainer} container d-flex flex-column gap-4`}
+        >
+          <div className="row">
+            <div className="col-12">
+              <DefaultOptions
+                places={defaultPlaces}
+                setCoordinates={setCoordinates}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? `col-6 ${styles.MapToggleButton} ${styles.active}`
+                  : `col-6 ${styles.MapToggleButton}`
+              }
+              to="/places"
+            >
+              Places
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? `col-6 ${styles.MapToggleButton} ${styles.active}`
+                  : `col-6 ${styles.MapToggleButton}`
+              }
+              to="/danger-zones"
+            >
+              Danger Zones
+            </NavLink>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              {/* <PlacesAutocomplte
                                 setCoordinates={setCoordinates}
                             /> */}
-                            <MuiAutofill setCoordinates={setCoordinates} />
-                        </div>
-                    </div>
-                    {pathname == "/places" ? (
-                        <div className="row">
-                            <div className="col-12">
-                                <Filter
-                                    filterSet={filterSet}
-                                    setSelectedFilters={setSelectedFilters}
-                                />
-                            </div>
-                        </div>
-                    ) : null}
-                    <div className="row">
-                        <div
-                            className={`col-12 ${styles.exitContainer} d-flex flex-column gap-2 align-items-center p-3`}
-                        >
-                            <AccountCircleIcon />
-                            <div>
-                                <p>User</p>
-                                <p className="text-muted">
-                                    testUser@example.com
-                                </p>
-                            </div>
-                            <NavLink to="../" className={styles.logOutBtn}>
-                                Exit
-                                <EastIcon className="" />
-                            </NavLink>
-                        </div>
-                    </div>
-                </div>
+              <MuiAutofill setCoordinates={setCoordinates} />
             </div>
+          </div>
+          {pathname == "/places" ? (
+            <div className="row">
+              <div className="col-12">
+                <Filter
+                  filterSet={filterSet}
+                  setSelectedFilters={setSelectedFilters}
+                />
+              </div>
+            </div>
+          ) : null}
+          <div className="row">
+            <div
+              className={`col-12 ${styles.exitContainer} d-flex flex-column gap-2 align-items-center p-3`}
+            >
+              <AccountCircleIcon />
+              <div>
+                <p>User</p>
+                <p className="text-muted">testUser@example.com</p>
+              </div>
+              <NavLink to="../" className={styles.logOutBtn}>
+                Exit
+                <EastIcon className="" />
+              </NavLink>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
